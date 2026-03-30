af866d18906ed83bba3e10e573972cb17325d444
DiegoArielAyala <diegoa.ayala@gmail.com>
2026-03-30 18:56:42 +0200

    Implement three_column_check and enhance check_posible_numbers_for_each_box with unique position logic

--- sudoku.py
+++ sudoku.py
@@ -57,12 +57,14 @@ def check_posible_numbers_for_each_box(round):
     number_filled_in_round = False
     print(f"Round: {round}")
     for row in enumerate(template):
+        row_index = row[0]
         for column in enumerate(row[1]):
-            row_index = row[0]
             column_index = column[0]
 
-            if type(template[row[0]][column_index]) == int:
+            if type(template[row_index][column_index]) == int:
+                #print(f"Working with number {template[row_index][column_index]} in row {row_index} and column {column_index}")
                 three_rows_check(row_index, column_index)
+                three_column_check(row_index, column_index)
                 continue
 
             posible_numbers = list(range(1, 10))
@@ -75,7 +77,9 @@ def check_posible_numbers_for_each_box(round):
             number_filled_in_round = check_unique_posible_number(posible_numbers, row_index, column_index, number_filled_in_round)
             # print(f"Posible numbers: {posible_numbers} for row {row_index + 1} and column {column_index + 1}")
 
+            """Nuevas funciones: agarrar cada fila (hacer lo mismo para columna), revisar que numeros faltan. Por cada numero que falta, revisar si hay un solo casillero en el que pueda ir, revisando si ese numero esta en cada columna de las casillas vacias o en los bloques de las casillas vacias """
             
+        unique_posible_position_in_row(row_index)
 
     print(f"is_sudoku_completed: {is_sudoku_completed()}")
     print(f"number_filled_in_round: {number_filled_in_round}")
@@ -84,6 +88,49 @@ def check_posible_numbers_for_each_box(round):
         check_posible_numbers_for_each_box(round)
     else:
         return
+
+def unique_posible_position_in_row(row_index):
+    posible_numbers_in_row = list(range(1, 10))
+    posible_positions = []
+    print(f"template row {row_index}: {template[row_index]}")
+    for column in enumerate(template[row_index]):
+        if type(column[1]) != int:
+            posible_positions.append(column[0])
+        if type(column[1]) == int:
+            try:
+                posible_numbers_in_row.remove(column[1])
+            except:
+                continue
+    if posible_numbers_in_row == []:
+        return
+    print(f"Posible numbers in row {row_index}: {posible_numbers_in_row}")
+    print(f"Posible positions in row {row_index}: {posible_positions}")
+    for posible_number in posible_numbers_in_row:
+        posible_positions_copy = posible_positions.copy()
+        print(f"Checking posible number {posible_number} in row {row_index}. Posible positions: {posible_positions_copy}")
+        # Revisar cada columna de las posiciones posibles, si el numero esta en esa columna, eliminar esa posicion de las posiciones posibles
+        # Revisar cada bloque de las posiciones posibles, si el numero esta en ese bloque, eliminar esa posicion de las posiciones posibles    
+        for column in [1, 4, 7]:
+            start_row_block, end_row_block, start_column_block, end_column_block = define_block(row_index, column)
+            if number_in_block(posible_number, start_row_block, end_row_block, start_column_block, end_column_block):
+                for column in range(start_column_block, end_column_block):
+                    try:
+                        posible_positions_copy.remove(column)
+                        print(f"1 Removed column {column} from posible positions. Posible positions left: {posible_positions_copy}")
+                    except:
+                        continue
+        for column in posible_positions_copy:
+            if number_in_column(posible_number, column):
+                try:
+                    posible_positions_copy.remove(column)
+                    print(f"2 Removed column {column} from posible positions. Posible positions left: {posible_positions_copy}")
+                except:
+                    continue
+        """Revisar"""
+        if len(posible_positions_copy) == 1 and review_number_before_fill() and type(template[row_index][posible_positions_copy[0]]) != int:
+            template[row_index][posible_positions_copy[0]] = posible_number
+            render_template()
+            print(f"Template in row {row_index} and column {posible_positions_copy[0]} = {posible_number} with unique posible position in row method")
 
 def check_rows_posible_numbers(posible_numbers, row_index):
     for box in template[row_index]:
@@ -152,41 +199,42 @@ def three_rows_check(row_index, column_index):
     # Si en solo una fila no esta el numero, revisar en los casilleros vacios, si en esas columnas esta el numero
     # Si solo hay un casillero vacio donde puede ir el numero, rellenarlo ahi
     number = template[row_index][column_index]
-    if type(template[row_index][column_index]) == int:
-        start_row_block, end_row_block, start_column_block, end_column_block = define_block(row_index, column_index)
+    
+    start_row_block, end_row_block, start_column_block, end_column_block = define_block(row_index, column_index)
 
-        counter = count_number_in_row_block(start_row_block, end_row_block, number)
-        
+    counter, positions_of_number = count_number_in_row_block(start_row_block, end_row_block, number)
+    
     # Si el counter es 2, buscar en que fila no esta el numero
-        if counter == 2:
-            # print(f"Working with number {template[row_index][column_index]} in row {row_index + 1} and column {column_index + 1}")
-            row = row_without_number(start_row_block, end_row_block, number)
- 
-    # Tomar esa fila, crear un rango del 0 al 8 con las columnas, eliminar cada numero si en esa casilla hay un numero o si en la columa existe el numero
-            columns = get_posibles_columns(row, number, start_column_block, end_column_block)
+    if counter == 2:
+        #print(f"Working with number {template[row_index][column_index]} in row {row_index + 1} and column {column_index + 1} using three_rows_method")
+        row = row_without_number(start_row_block, end_row_block, number)
+
+        # Tomar esa fila, crear un rango del 0 al 8 con las columnas, eliminar cada numero si en esa casilla hay un numero o si en la columa existe el numero
+        columns = get_posibles_columns(row, number, positions_of_number)
+        fill_number_if_one_column_left(row, columns, number)
+
+        # Hay que buscar en que bloque esta el otro numero, no "number", sino el otro
+        try:
+            row_of_second_number = get_row_of_second_number(start_row_block, end_row_block, row_index, number)
+            number_index = template[row].index(number)
+            #print(f"number index {number_index}")
+            _, _, start_column_block, end_column_block = define_block(row_of_second_number, number_index)
             
-# Hay que buscar en que bloque esta el otro numero, no "number", sino el otro
-            try:
-                row = row_of_second_number(start_row_block, end_row_block, row_index, number)
-                number_index = template[row].index(number)
-                #print(f"number index {number_index}")
-                _, _, start_column_block, end_column_block = define_block(row, number_index)
-                
-                for column in range(start_column_block, end_column_block):
-                    #print(f"Column {column}")
-                    try:
-                        columns.remove(column)
-                        #print(f"4 Removed column {column}. Columns left: {columns}")
-                    except:
-                        continue
-                fill_number_if_one_column_left(row, columns, number)
-            except:
-                return
-            # print(f"Posible columns for number {number} in row {row + 1}: {columns}")
-
+            for column in range(start_column_block, end_column_block):
+                #print(f"Column {column}")
+                try:
+                    columns.remove(column)
+                    #print(f"4 Removed column {column}. Columns left: {columns}")
+                except:
+                    continue
             fill_number_if_one_column_left(row, columns, number)
+        except:
+            return
+        # print(f"Posible columns for number {number} in row {row + 1}: {columns}")
 
-def get_posibles_columns(row, number, start_column_block, end_column_block):
+        fill_number_if_one_column_left(row, columns, number)
+
+def get_posibles_columns(row, number, positions_of_number):
     columns = list(range(0, 9))
     for column in range(0, 9):
         if type(template[row][column]) is int:
@@ -203,35 +251,46 @@ def get_posibles_columns(row, number, start_column_block, end_column_block):
             except:
                 continue
 # Eliminar las columas del bloque donde esta number
-    for column in range(start_column_block, end_column_block):
-        try:
-            columns.remove(column)
-            #print(f"3 Removed column {column}. Columns left: {columns}")
-        except:
-            continue
+    for position in positions_of_number:
+        row_index = position[0]
+        column_index = position[1]
+        start_row_block, end_row_block, start_column_block, end_column_block = define_block(row_index, column_index)
+        if number_in_block(number, start_row_block, end_row_block, start_column_block, end_column_block):
+            for column in range(start_column_block, end_column_block):
+                try:
+                    columns.remove(column)
+                    #print(f"3 Removed column {column}. rows left: {columns}")
+                except:
+                    continue
+            #print(f"columns: {columns}")
+    
     return columns
 
 def count_number_in_row_block(start_row_block, end_row_block, number):
     counter = 0
+    positions_of_number= []
     for row in range(start_row_block, end_row_block):
-            if number in template[row]:
-                counter += 1
-    # print(f"Number {number} is in row range {start_row + 1} to {end_row} {counter} times")
-    return counter
+        if number in template[row]:
+            column = template[row].index(number)
+            counter += 1
+            positions_of_number.append([row, column])
+    #print(f"Number {number} is in row range {start_row_block} to {end_row_block - 1} {counter} times")
+    #print(f"positions_of_number {number}: {positions_of_number}")
+    return counter, positions_of_number
 
 def row_without_number(start_row_block, end_row_block, number):
     for row in range(start_row_block, end_row_block):
         if number not in template[row]:
+            #print(f"Number {number} not in row {row}")
             return row
-    # print(f"Number {number} not in row {row + 1}")
 
 def fill_number_if_one_column_left(row, columns, number):
-    if len(columns) == 1 and review_number_before_fill():
+    if len(columns) == 1 and review_number_before_fill() and type(template[row][columns[0]]) != int:
         template[row][columns[0]] = number
         render_template()
-        print(f"Template in row {row + 1} and column {columns[0] + 1} = {number} with three rows check method")
+        print(f"Template in row {row} and column {columns[0]} = {number} with three rows check method")
 
-def row_of_second_number(start_row_block, end_row_block, row_index, number):
+def get_row_of_second_number(start_row_block, end_row_block, row_index, number):
     rows = list(range(start_row_block, end_row_block))
     try:
         # Remover row del number
@@ -253,17 +312,153 @@ def number_is_in_row(number, row_index):
     return number in template[row_index]
      
 
-def three_column_check():
-    pass
+def three_column_check(row_index, column_index):
+    # Agarrar cada celda
+    # Revisar si en una de las filas de su bloque esta ese numero
+    # Revisar en la otra fila de ese bloque si esta ese numero
+    # Si en solo una fila no esta el numero, revisar en los casilleros vacios, si en esas columnas esta el numero
+    # Si solo hay un casillero vacio donde puede ir el numero, rellenarlo ahi
+    number = template[row_index][column_index]
+
+    start_row_block, end_row_block, start_column_block, end_column_block = define_block(row_index, column_index)
+
+    counter, positions_of_number = count_number_in_column_block(start_column_block, end_column_block, number)
+
+    # Si el counter es 2, buscar en que columna no esta el numero
+    if counter == 2:
+        #print(f"Working with number {template[row_index][column_index]} in row {row_index} and column {column_index} using three_column_method")
+        column = column_without_number(start_column_block, end_column_block, number)
+        # Tomar esa columna, crear un rango del 0 al 8 con las filas, eliminar cada numero si en esa casilla hay un numero o si en la columa existe el numero
+        rows = get_posibles_rows(column, number, positions_of_number)
+        fill_number_if_one_row_left(column, rows, number)
+        
+        # Hay que buscar en que bloque esta el otro numero, no "number", sino el otro
+        try:
+            column_of_second_number, row_of_second_number = get_column_of_second_number(start_column_block, end_column_block, column_index, number)
+            
+            #print(f"number index {number_index}")
+            start_row_block, end_row_block, _, _ = define_block(row_of_second_number, column_of_second_number)
+            
+            for row in range(start_row_block, end_row_block):
+                #print(f"Column {column}")
+                try:
+                    rows.remove(row)
+                    #print(f"4 Removed column {row}. Columns left: {rows}")
+                except:
+                    continue
+            fill_number_if_one_row_left(column, rows, number)
+        except:
+            return
+        # print(f"Posible columns for number {number} in row {row + 1}: {columns}")
+
+        fill_number_if_one_row_left(column, rows, number)
+
+
+def fill_number_if_one_row_left(column, rows, number):
+    if len(rows) == 1 and review_number_before_fill() and type(template[rows[0]][column]) != int:
+        template[rows[0]][column] = number
+        render_template()
+        #print(f"Template in row {rows[0]} and column {column} = {number} with three rows check method")
+
+
+def get_column_of_second_number(start_column_block, end_column_block, column_index, number):
+    columns = list(range(start_column_block, end_column_block))
+    try:
+        # Remover row del number
+        columns.remove(column_index)
+        # print(f"Removed row {row_index}")
+    except:
+        return
+    
+    for column in columns:
+        for row in range(0, 9):
+            if number in template[row][column]:
+                return column, row    
+
+def count_number_in_column_block(start_column_block, end_column_block, number):
+    counter = 0
+    positions_of_number= []
+    for column in range(start_column_block, end_column_block):
+        for row in range(0, 9):
+            if number == template[row][column]:
+                counter += 1
+                positions_of_number.append([row, column])
+    #print(f"Number {number} is in column range {start_column_block} to {end_column_block - 1} {counter} times")
+    #print(f"positions_of_number {number}: {positions_of_number}")
+    return counter, positions_of_number
+
+
+def column_without_number(start_column_block, end_column_block, number):
+    for column in range(start_column_block, end_column_block):
+        # print(f"Column: {column}, number {number}, start_column_block {start_column_block}, end_column_block {end_column_block}")
+        numbers_in_column = []
+        for row in range(0, 9):
+            if type(template[row][column]) == int:
+                numbers_in_column.append(template[row][column])
+        if number not in numbers_in_column:
+            #print(f"Number {number} not in column {column}")
+            return column
+
+
+def get_posibles_rows(column, number, positions_of_number):
+    rows = list(range(0, 9))
+    for row in range(0, 9):
+        if type(template[row][column]) is int:
+            try:
+                rows.remove(row)
+                #print(f"1 Removed row {row}. rows left: {rows}")
+            except:
+                continue
+        # Revisar fila
+        if number_in_row(number, row):
+            try:
+                rows.remove(row)
+                #print(f"2 Removed row {row}. rows left: {rows}")
+            except:
+                continue
+# Eliminar las filas del bloque donde esta number
+    
+
+    """ Agregar que se eliminen las filas del bloque donde se repite number y hacer lo equivalente en get_posibles_columns """
+    """ Esta mal hecha la funcion porque solo revisa el bloque que ya tiene el numero y necesito que revise los 3 bloques con los que estoy trabajando """
+    for position in positions_of_number:
+        row_index = position[0]
+        column_index = position[1]
+        start_row_block, end_row_block, start_column_block, end_column_block = define_block(row_index, column_index)
+        if number_in_block(number, start_row_block, end_row_block, start_column_block, end_column_block):
+            for row in range(start_row_block, end_row_block):
+                try:
+                    rows.remove(row)
+                    #print(f"3 Removed row {row}. rows left: {rows}")
+                except:
+                    continue
+            #print(f"Rows: {rows}")
+    return rows
+
+
+def number_in_row(number, row):
+    if number in template[row]:
+        return True
+
+
+def number_in_block(number, start_row_block, end_row_block, start_column_block, end_column_block):
+    for row in range(start_row_block, end_row_block):
+        for column in range(start_column_block, end_column_block):
+            if number == template[row][column]:
+                #print(f"Number {number} is in block of rows {start_row_block} - {end_row_block} and columns {start_column_block} - {end_column_block}")
+                return True
+
+
+
 
 def check_unique_posible_number(posible_numbers, row_index, column_index, number_filled_in_round):
-    if len(posible_numbers) == 1 and type(posible_numbers[0]) == int:
-        if review_number_before_fill():
-            template[row_index][column_index] = posible_numbers[0]
-            render_template()
-            print(f"Template in row {row_index + 1} and column {column_index + 1} = {posible_numbers[0]} with unique posible number method")
-            number_filled_in_round = True
-            return number_filled_in_round
+    #print(f"Check unique posible number for row {row_index} column {column_index}")
+    if len(posible_numbers) == 1 and type(posible_numbers[0]) == int and review_number_before_fill():
+        template[row_index][column_index] = posible_numbers[0]
+        render_template()
+        print(f"Template in row {row_index + 1} and column {column_index + 1} = {posible_numbers[0]} with unique posible number method")
+        number_filled_in_round = True
+        return number_filled_in_round
     return number_filled_in_round
 
 def review_number_before_fill():
